cab0de2badd2c7133da2ed4684d5c585d5dd2f8c
Zoei <taojun1991@126.com>
2020-12-26 16:18:24 +0800

    navbar

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="nav-bar">
+    <div class="nav-left"><slot name="left"></slot></div>
+    <div class="nav-center"><slot name="center"></slot></div>
+    <div class="nav-right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.nav-bar {
+  position: relative;
+  display: flex;
+  text-align: center;
+  height: 44px;
+  line-height: 44px;
+  width: 100%;
+  background-color: #ff8198;
+  z-index: 10;
+  color: #fff;
+  font-size: 16px;
+}
+
+.nav-left, .nav-right{
+  width: 60px;
+}
+
+.nav-center {
+  flex: 1
+}
+</style>
---
+++ src/components/content/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nav-bar>
+      <span slot="left">返回</span>
+      <span slot="center">购物街</span>
+      <span slot="right">我的</span>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from 'common/navbar/NavBar'
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+
+</style>
